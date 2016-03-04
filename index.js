bfa4a1a95e9a5917d25339210bc844a1d0c74119
twentychung <twentychung@163.com>
2016-03-04 14:53:37 +0800

    no message

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 //vi fis3-phalcon/index.js
-var fis = module.exports =  require('fis3');
+var fis = module.exports = require('fis3');
 
 fis.require.prefixes.unshift('fis3-phalcon');
 fis.cli.name = 'fis3-phalcon';
 fis.cli.info = require('./package.json');
 
 fis.match('*', {
-  release: '/public/$0' // 所有资源发布时产出到 /static 目录下
+    release: '/public/$0' // 所有资源发布时产出到 /public 目录下
 });
 
 fis.match('*.volt', {
-  release: '/app/views/$0' // 所有 PHP 模板产出后放到 /app/views 目录下
+    release: '/app/views/$0' // 所有 PHP 模板产出后放到 /app/views 目录下
 });
 
 // 所有js, css 加 hash
 fis.match('*.{js,css,less}', {
-  useHash: true
+    useHash: true
 });
 
 // 所有图片加 hash
 fis.match('image', {
-  useHash: true
+    useHash: true
 });
 
 // fis-parser-less
 fis.match('*.less', {
-  parser: fis.plugin('less'),
-  rExt: '.css'
+    parser: fis.plugin('less'),
+    rExt: '.css'
 });
 
 fis.match('*.js', {
-  optimizer: fis.plugin('uglify-js')
+    optimizer: fis.plugin('uglify-js')
 });
 
 fis.match('*.{css,less}', {
-  optimizer: fis.plugin('clean-css')
+    optimizer: fis.plugin('clean-css')
 });
 
 fis.match('*.png', {
-  optimizer: fis.plugin('png-compressor')
+    optimizer: fis.plugin('png-compressor')
 });
 
 fis.match('widget/*.{php,js,css}', {
-  isMod: true
+    isMod: true
 });
 
 fis.match('::package', {
-  spriter: fis.plugin('csssprites')
+    spriter: fis.plugin('csssprites')
 });
 
 //fis3-hook-module
